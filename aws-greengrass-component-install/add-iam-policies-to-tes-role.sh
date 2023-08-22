7446d1dc56b8a01c02af7ab953ba33620f6999d8
David Malone <dmmal@amazon.com>
2023-08-22 08:13:15 -0400

    fixed file path for command to attach KVS Edge access IAM policy to GG TES IAM Role

--- aws-greengrass-component-install/add-iam-policies-to-tes-role.sh
+++ aws-greengrass-component-install/add-iam-policies-to-tes-role.sh
@@ -6,5 +6,5 @@ IAM_ROLE=KvsEdgeGreengrassV2TokenExchangeRole
 if aws iam get-role-policy --role-name $IAM_ROLE --policy-name $IAM_POLICY 2>&1 | grep -q 'NoSuchEntity'; then
   echo "IAM Role Policy $IAM_POLICY does not exist; creating now..."
   aws iam put-role-policy --role-name $IAM_ROLE \
-    --policy-name $IAM_POLICY --policy-document 'file://iot/iam-policy-document.json'
+    --policy-name $IAM_POLICY --policy-document 'file://iam-policy-document.json'
 fi
